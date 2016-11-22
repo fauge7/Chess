8de5d46e3b516bae66b722004b61d4acc3b0b839
fauge7 <biwydakid50@gmail.com>
2016-11-22 14:42:38 -0700

    made pupup buttons centered

--- src/com/group8/chess/server/Server.java
+++ src/com/group8/chess/server/Server.java
@@ -1,5 +1,17 @@
 package com.group8.chess.server;
 
+import javax.swing.Popup;
+
+import com.group8.chess.util.PopUp;
+import com.group8.chess.util.PopupType;
+
 public class Server {
 	// Use board.buildMoveList() once each time the player changes.
+	public static void main(String[] args) {
+		Server server = new Server();
+	}
+	public Server() {
+		// TODO Auto-generated constructor stub
+		PopUp up = new PopUp(PopupType.WHITE_TURN);
+	}
 }
--- src/com/group8/chess/util/PopUp.java
+++ src/com/group8/chess/util/PopUp.java
@@ -1,78 +1,72 @@
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Font;
+package com.group8.chess.util;
 
-public class PopUp
-{
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import org.omg.PortableServer.ServantRetentionPolicyValue;
+
+public class PopUp {
     //variables for buttons
     private JFrame frame;
     private JTextField txtButton;
-    private JTextField txtWhite;
-    private JTextField txtBlack;
-    private JTextField txtCheck;
-    private JTextField txtCMate;
-    private JTextField txtSMate;
-    private JTextField txtConnect;
+    private JTextField txt;
     
     //constructer to test button
-    public PopUp()
+    public PopUp(PopupType type)
     {
-        basicButton();
+    	switch (type) {
+		case CONNECTING:
+			
+			break;
+		case WHITE_TURN:
+			whiteTurn();
+			break;
+		case BLACK_TURN:
+			
+			break;
+		case CHECK:
+			
+			break;
+		case CHECKMATE:
+			
+			break;
+		default:
+			break;
+		}
+    	frame.setVisible(true);
         //whiteTurn();
     }
     
     //main method builds button
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    PopUp window = new PopUp();
-                    window.frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        
     }
     //button template
     private void basicButton() {
        frame = new JFrame();
+       frame.setVisible(true);
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
+        
+        txt = new JTextField("");
+        txt.setEditable(false);
+        txt.setBounds(frame.getWidth()/2, 21, 200, 20);
+        txt.setBounds(frame.getWidth()/2-txt.getWidth()/2, 21, txt.getWidth(), txt.getHeight());
+        frame.getContentPane().add(txt);
+        txt.setColumns(10);
         
         txtButton = new JTextField();
         txtButton.setText("Button");
-        txtButton.setBounds(180, 20, 85, 20);
+        txtButton.setBounds(frame.getWidth()/2-85/2, 20, 85, 20);
         frame.getContentPane().add(txtButton);
         txtButton.setColumns(10);
-
-        JButton btnOk = new JButton("OK");
-        btnOk.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                frame.dispose();
-            }
-        });
-        btnOk.setBounds(166, 159, 89, 23);
-        frame.getContentPane().add(btnOk);      
-    }
-    private void whiteTurn() {
-        frame = new JFrame();
-        frame.setBounds(100, 100, 450, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(null);
-        
-        txtWhite = new JTextField();
-        txtWhite.setText("Player White Turn");
-        txtWhite.setBounds(180, 20, 85, 20);
-        frame.getContentPane().add(txtWhite);
-        txtWhite.setColumns(10);
-
+        txt.setText("Player White Turn");
         JButton btnOk = new JButton("OK");
         btnOk.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
@@ -81,6 +75,11 @@ public class PopUp
         });
         btnOk.setBounds(166, 159, 89, 23);
         frame.getContentPane().add(btnOk);     
+    }
+    private void whiteTurn() {
+        basicButton();
+        txt.setText("White turn");
+    	
     }
     private void blackTurn() {
         frame = new JFrame();
